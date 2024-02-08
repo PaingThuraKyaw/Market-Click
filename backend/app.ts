737fdaefab46f7d5f6b4a984e0114bf4fe430513
Boris <72118807+Curis-lab@users.noreply.github.com>
2024-02-08 22:20:12 +0630

    add initialze file

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,7 @@
 import express from "express";
+import category from "./initData/category";
 
 const app = express();
-
-interface IProduct {
-  id: string;
-  title: string;
-  imageUrl: string;
-}
 
 //register views engin
 app.set("view engine", "ejs");
@@ -14,16 +9,11 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.sendFile("./views/index.html", { root: __dirname });
 });
+
 app.get("/api/shopping", (req, res) => {
-  const products: IProduct[] = [
-    {
-      id: "123",
-      title: "Phone",
-      imageUrl: "https://i3einter",
-    },
-  ];
-  res.json(products);
+  res.json(category);
 });
+
 app.listen(3000, function () {
   console.log("running on port 3000");
 });
---
+++ backend/initData/category.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: "123",
+    title: "Phone",
+    imageUrl: "https://i3einter",
+  },
+  {
+    id: "123",
+    title: "Phone",
+    imageUrl: "https://i3einter",
+  },
+  {
+    id: "123",
+    title: "Phone",
+    imageUrl: "https://i3einter",
+  },
+];

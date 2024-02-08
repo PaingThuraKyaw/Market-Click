0737bc6358064c81401ab04a7fcceada9b604505
Boris <72118807+Curis-lab@users.noreply.github.com>
2024-02-08 08:19:15 +0630

    may start using it

--- backend/README.md
+++ backend/README.md
@@ -10,3 +10,10 @@ Before running the project, please ensure that the following applications are in
 npm I
 npm run dev
 ```
+
+
+### you may start using it
+
+```js
+fetch('/http://localhost:3000/api/shopping');
+```
--- backend/app.ts
+++ backend/app.ts
@@ -1,1 +1,29 @@
-console.log('Start TS');
+import express from "express";
+
+const app = express();
+
+interface IProduct {
+  id: string;
+  title: string;
+  imageUrl: string;
+}
+
+//register views engin
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+app.get("/api/shopping", (req, res) => {
+  const products: IProduct[] = [
+    {
+      id: "123",
+      title: "Phone",
+      imageUrl: "https://i3einter",
+    },
+  ];
+  res.json(products);
+});
+app.listen(3000, function () {
+  console.log("running on port 3000");
+});
